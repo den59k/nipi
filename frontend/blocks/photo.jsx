f79909b800e812bcf8a39951fc125f827fb04455
den59k <den59k@gmail.com>
2020-12-25 07:26:59 +0500

    a

--- frontend/blocks/photo.jsx
+++ frontend/blocks/photo.jsx
@@ -36,6 +36,7 @@ export default function PhotoGalleryBlock ({ photos }){
 
 	const [ currentPhoto, setCurrentPhoto ] = useState(0)
 	const count = useResize(resizing)
+	const [ page, setPage ] = useState(0)
 
 	const onPhotoClick = (index) => {
 		setCurrentPhoto(index)
@@ -46,9 +47,19 @@ export default function PhotoGalleryBlock ({ photos }){
 			const json = await fetch('/api/photos/upload', { method: 'POST', body: buffer })
 			const response = await json.json()
 			mutate('/api')
+			if(photos.length >= count*2)
+				setPage(Math.floor((photos.length)/count/2)*2)
+
 			closeModal()
 		})
 	}	
+
+	const slide = (inc) => {
+		const newPage = page + inc
+		if(newPage < 0 || newPage*count >= photos.length) return
+
+		setPage(newPage)
+	}
 
 	return (
 		<div className={cn("h flex", styles.background)} id="photo-gallery">
@@ -56,13 +67,17 @@ export default function PhotoGalleryBlock ({ photos }){
 			<h2>Новогодняя фотогалерея</h2>
 			<div className={cn("content container", styles.container)}>
 				<div className={styles.photosContainer}>
-					<div className={styles.photos}>{getPhotos(photos, 0, count, onPhotoClick)}</div>
+					<div className={styles.photos}>{getPhotos(photos, page*count, (page+1)*count, onPhotoClick)}</div>
 					<div className={styles.bigPhoto} style={{
 						backgroundImage: `url(${photos[currentPhoto]?photos[currentPhoto].src:'/images/preview-gallery.jpg'
 					})`}}></div>
-					<div className={styles.photos}>{getPhotos(photos, count, count*2, onPhotoClick)}</div>
+					<div className={styles.photos}>{getPhotos(photos, (page+1)*count, (page+2)*count, onPhotoClick)}</div>
 				</div>
-				<button className={cn("mega-button", styles.button)} onClick={onMakePhoto}>Сделать праздничное фото</button>
+				<div className={styles.button}>
+					<button className={cn(styles.leftButton, page === 0 && styles.hide)} onClick={() => slide(-2)}></button>
+					<button className={cn("mega-button", styles.button)} onClick={onMakePhoto}>Сделать праздничное фото</button>
+					<button className={cn(styles.rightButton, count*(page+2) >= photos.length && styles.hide)} onClick={() => slide(2)}></button>
+				</div>
 			</div>
 		</div>
 	)
